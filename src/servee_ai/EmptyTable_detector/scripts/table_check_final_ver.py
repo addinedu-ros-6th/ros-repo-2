12b25e15a373b1a1f285e96d9f1e2c4f4220cea3
YoonHee1000 <gmlcjs1018@gmail.com>
2024-11-19 09:43:47 +0900

    update: upgrade performance table model SVRB-22

--- src/servee_ai/EmptyTable_detector/scripts/table_check_final_ver.py
+++ src/servee_ai/EmptyTable_detector/scripts/table_check_final_ver.py
@@ -11,7 +11,7 @@ from std_msgs.msg import String
 
 # 학습된 모델 로드
 # model = tf.keras.models.load_model('./servee_ai/EmptyTable_detector/scripts/table_check_model_final_ver.keras')
-model = tf.keras.models.load_model('/home/heechun/dev_ws/ros-repo-2/src/servee_ai/EmptyTable_detector/scripts/table_check_model_final_ver.keras')
+model = tf.keras.models.load_model('/home/heechun/dev_ws/ros-repo-2/src/servee_ai/EmptyTable_detector/scripts/1118_model_01.keras')
 
 # 테이블 ROI 설정: (x, y, width, height) 형식으로 4개의 테이블 지정
 # table_rois = {
