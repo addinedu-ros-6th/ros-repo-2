5fd715deb7a526e9e09f5f2acb133ab41dd0b4d6
Jaechang <kjc06150@gmail.com>
2024-11-20 20:21:56 +0900

    bugFix: socket reusea address SVRB-34

--- src/servee_server/servee_server/observer_publisher.py
+++ src/servee_server/servee_server/observer_publisher.py
@@ -102,7 +102,6 @@ class Server:
 
     def start(self):
         self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.server_socket.bind((self.host, self.port))
         self.server_socket.listen(5)
         print(f"Server running on {self.host}:{self.port}...")
